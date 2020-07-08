aa745c2c3d8bb7dcc98394cd7b63b96f52f39903
James Komo <47238834+jameskomo@users.noreply.github.com>
2020-07-08 11:36:26 +0300

    Bash Script Created

---
+++ sortfiles.sh
@@ -0,0 +1,33 @@
+
+# write your code in Bash
+
+#! /bin/bash
+
+## create the directory, -p means it will create parent dirs
+## and not complain if the file already exists
+
+DIR=$(pwd)
+images="$DIR/images"
+mkdir -p "$images"
+videos="$DIR/videos"
+mkdir -p "$videos"
+music="$DIR/music"
+mkdir -p "$music"
+
+for file in $DIR/*
+do
+	## basename removes file path and remove the extension if
+	## a second arg is provided
+	name="$(basename "$file")"
+	if [ "${name##*.}" == "jpg" ] || [ "${name##*.}" == "png" ] ; then
+		mv "$file" "images"
+	elif [ "${name##*.}" == "mp3" ] || [ "${name##*.}" == "flac" ] ; then
+		mv "$file" "$music"
+    elif [ "${name##*.}" == "avi" ] || [ "${name##*.}" == "mov" ] ; then
+		mv "$file" "$videos"
+	elif [ "${name##*.}" == "log" ] ; then
+		rm "$file" 
+	else
+		true
+	fi
+done
